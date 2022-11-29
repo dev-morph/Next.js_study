f0b1b2791e5508c6bb84ef304bfef789ab0418bb
dev-morph <112484043+dev-morph@users.noreply.github.com>
2022-11-30 00:06:14 +0900

    test

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout/layout'
 
 function MyApp({ Component, pageProps }) {
   return (
+    //Jandi Teset
     <Layout>
       <Component {...pageProps} />
     </Layout>

d129641ef84850257a73029310d709101164bac5
dev-morph <112484043+dev-morph@users.noreply.github.com>
2022-11-29 23:49:37 +0900

    a

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,6 +16,7 @@ function HomePages(props) {
 export async function getStaticProps() {
   // cwd를 사용하게 되면, 현재 위치 pages 폴더 안이 아닌,
   // build가 된 이후, root폴더에 있는 것처럼 취급되게 된다.
+  // fs logic...
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData)
